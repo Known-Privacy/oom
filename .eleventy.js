92e8a7366141c0a4bd7395cb9cf53eb38f3cf3d6
Brendan Roberts <brendan@knownprivacy.com>
2022-10-25 11:03:53 -0600

    build image paths

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,24 @@
 const htmlmin = require('html-minifier')
 const now = String(Date.now())
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 600],
+    formats: ["avif", "jpeg", "jpg", "png"],
+    useCache: true
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
@@ -8,6 +27,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addShortcode('version', function () {
     return now
   })
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
 
   eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
     if (

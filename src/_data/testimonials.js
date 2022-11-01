5cb4992eb44a7bf3217393fd6c7400a3365127f8
Brendan Roberts <brendan@knownprivacy.com>
2022-11-01 16:06:43 -0600

    changes about us hero; adds testimonials on home

---
+++ src/_data/testimonials.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        message: "I am heartened to see I already have an opt-out response back from one of the companies, with data. It's great to be able to see and download directly the spreadsheet they sent.",
+        author: "K.L."
+    },
+    {
+        message: "The satisfaction of seeing the opt-out requests sent to these companies was well worth the $100 for Known Privacy's Opt-Out Machine. Now the companies have 45 days to comply and must acknowledge the request within 15 days.",
+        author: "Roman V."
+    },
+    {
+        message: "So far I rate this as a five. It is nice to know that there is an option for those of use who value data security. Thank you for giving us a chance!",
+        author: "David S."
+    }
+];

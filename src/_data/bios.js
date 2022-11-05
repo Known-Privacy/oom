81937a43eafd48124076dc3e00ffef47b605467b
Brendan Roberts <brendan@knownprivacy.com>
2022-11-04 19:40:14 -0600

    adds founder bios

---
+++ src/_data/bios.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        name: "John Ruder",
+        title: "Co-founder & CEO",
+        picture: "/assets/images/john-ruder.jpg",
+        linkedInUrl: "https://www.linkedin.com/in/johnruder/",
+        twitterUrl: "https://twitter.com/JohnRuder",
+        bio: "content/john.html",
+    },
+    {
+        name: "Brendan Roberts",
+        title: "Co-founder & CTO",
+        picture: "/assets/images/brendan-roberts.jpeg",
+        linkedInUrl: "https://www.linkedin.com/in/brendan-roberts-22092a21/",
+        twitterUrl: "https://twitter.com/brendanmroberts",
+        bio: "content/brendan.html",
+    },
+];

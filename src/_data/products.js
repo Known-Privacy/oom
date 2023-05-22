ff38dffee0b213a72d99443fee2186febea85370
Brendan Roberts <brendan@knownprivacy.com>
2023-05-22 13:46:52 -0600

    revises pricing page layout (simple)

---
+++ src/_data/products.js
@@ -0,0 +1,28 @@
+module.exports = [
+  {
+    name: "Opt-Out Machine DIY",
+    description: "Opt-outs to over 300 surveillance companies",
+    price: "$30",
+    link: "https://app.knownprivacy.com/users/register?tier=self",
+    features: [
+      "Automation tooling for you to handle communication with Data Brokers",
+      "Less spam, fewer calls, and fewer scams",
+      "Email, Chat, and Phone Support",
+      "Money-back guarantee",
+    ]
+  },
+  {
+    name: "Opt-Out Machine",
+    description: "Automated opt-out requests",
+    price: "$120",
+    link: "https://app.knownprivacy.com/users/register",
+    features: [
+      "All of the Data Brokers in the DIY plan",
+      "Automatic handling of communication",
+      "Save over 40 hours of work vs doing it on your own",
+      "Less spam, fewer calls, and fewer scams",
+      "Email, Chat, and Phone Support",
+      "Money-back guarantee",
+    ]
+  },
+];

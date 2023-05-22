e719a00c036b86084928a3dbcbd75a7bb3685a1f
Brendan Roberts <brendan@knownprivacy.com>
2023-05-22 13:10:45 -0600

    pricing adjustments

--- src/_data/features.js
+++ src/_data/features.js
@@ -6,9 +6,8 @@ module.exports = {
     "Money-back guarantee",
   ],
   fullServe: [
-    "Copies of the data these companies have about you",
-    "Save over 40 hours of work vs doing it on your own",
     "Automatic handling of communication with Data Brokers",
+    "Save over 40 hours of work vs doing it on your own",
     "Less spam, fewer calls, and fewer scams",
     "Email, Chat, and Phone Support",
     "Money-back guarantee",
